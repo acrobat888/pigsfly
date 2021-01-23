744ec560e48a8e63dc6a2c3e11ff40abbb483a34
Brad Post <brad.post@synapsefi.com>
2021-01-22 16:21:05 -0800

    Changes made to get cimages class working.

--- typescript/pigsfly.ts
+++ typescript/pigsfly.ts
@@ -5,6 +5,7 @@
 //  Lots of help from Sadie's dad...
 //  --------
 
+
 // The function gets called when the window is fully loaded
 window.onload = function() {
 
@@ -12,6 +13,30 @@ window.onload = function() {
     var cvs: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement;
     var ctx: CanvasRenderingContext2D = cvs.getContext("2d");
 
+    var images: Array<HTMLImageElement> = [];
+    var cbird : CImages = null;
+    var cbg : CImages = null;
+    var cfg : CImages = null;
+    var cpipeNorth: CImages = null;
+    var cpipeSouth: CImages = null;
+    
+    var bg: HTMLImageElement = null;
+    var bird: HTMLImageElement = null;
+    var fg: HTMLImageElement = null;
+    var pipeNorth: HTMLImageElement = null;
+    var pipeSouth: HTMLImageElement = null;
+    
+    // Image loading global variables
+    var imageNames: Array<string> = [   "images/bird.png",
+                                        "images/bg.png",
+                                        "images/fg.png",
+                                        "images/pipeNorth.png",
+                                        "images/pipeSouth.png" ];
+
+    var loadcount = 0;
+    var loadtotal = imageNames.length;
+    var preloaded = false;
+    
     // load images
     // ImageClass
     class CImages
@@ -27,29 +52,101 @@ window.onload = function() {
             this.file = inFile;
         }
 
-    
         load()
         {
             this.image = new Image();
+            var self = this;
 
             // Add onload event handler
-            this.image.onload = function () {
+            this.image.onload = function() {
+                console.log(self.name + " was loaded");
+                self.loaded = true;
+
+                // Done loading
+                // this.loaded = true;
+                /*
+                var tgt: HTMLImageElement = (that.target as HTMLImageElement);
+                var str = tgt.src;
+
+                console.log( tgt );
+
+                if (str.endsWith("images/bird.png"))
+                    console.log("Found a bird");
+
+                // check to see if the object isn't loaded, if so, mark it loaded
+                */
+                loadcount++;
+
+                if (loadcount == loadtotal) {
                     // Done loading
-                    // this.loaded = true;
+                    preloaded = true;
+                }
+
+                return true;
             };
-            
+
             this.image.src = this.file;
+        }
+
+        getImage()
+        {
+            return this.image;
         }
     }
 
-    var images: Array<HTMLImageElement> = [];
-    var bg: HTMLImageElement = null;
-    var bird: HTMLImageElement = null;
-    var fg: HTMLImageElement = null;
-    var pipeNorth: HTMLImageElement = null;
-    var pipeSouth: HTMLImageElement = null;
+    function loadIndividualImages() 
+    {
+        if (cbg == null)
+        {
+            cbg = new CImages( "bg", "images/bg.png")
+            if (cbg != null)
+            {
+                cbg.load();
+                bg = cbg.getImage();
+            }
+        }
+                
+        if (cfg == null)
+        {
+            cfg = new CImages( "fg", "images/fg.png")
+            if (cfg != null)
+            {
+                cfg.load();
+                fg = cfg.getImage();
+            } 
+        }
 
-    function loadIndividualImages() {
+        if (cbird == null)
+        {
+            cbird = new CImages( "bird", "images/bird.png");
+            if (cbird != null)
+            {
+                cbird.load();
+                bird = cbird.getImage();
+            }
+        }
+
+        if (cpipeNorth == null)
+        {
+            cpipeNorth = new CImages( "pipeNorth", "images/pipeNorth.png");
+            if(cpipeNorth != null)
+            {
+                cpipeNorth.load();
+                pipeNorth = cpipeNorth.getImage();
+            }
+        }
+
+        if (cpipeSouth == null)
+        {
+            cpipeSouth = new CImages( "pipeSouth", "images/pipeSouth.png");
+            if(cpipeSouth != null)
+            {
+                cpipeSouth.load();
+                pipeSouth = cpipeSouth.getImage();
+            }
+        }
+
+            /*
         bird = new Image();
         bird.src = "images/bird.png";     
 
@@ -64,18 +161,9 @@ window.onload = function() {
 
         pipeSouth = new Image();
         pipeSouth.src = "images/pipeSouth.png";
+        */
     }
 
-    // Image loading global variables
-    var loadcount = 0;
-    var loadtotal = 0;
-    var preloaded = false;
-    
-    var imageNames: Array<string> = [   "images/bird.png",
-                                        "images/bg.png",
-                                        "images/fg.png",
-                                        "images/pipeNorth.png",
-                                        "images/pipeSouth.png" ];
     // Load images
     function loadImages(imagefiles) {
         // Initialize variables
@@ -92,15 +180,6 @@ window.onload = function() {
             // Add onload event handler
             image.onload = function (inEvent: Event) {
                 loadcount++;
-
-                // typecast to get to the src
-                var tgt: HTMLImageElement = (inEvent.target as HTMLImageElement);
-                var str = tgt.src;
-
-                console.log( tgt );
-
-                if (str.endsWith("images/bird.png"))
-                    console.log("Found a bird");
 
                 if (loadcount == loadtotal) {
                     // Done loading
@@ -216,11 +295,11 @@ window.onload = function() {
 
     // draw images
 
-    function draw(){
-        
+    function draw()
+    {    
         if (!pauseGame && preloaded)
         {
-            for( var i = 0; i < cvs.width; i+= bg.width)
+            for( var i = 0; i < cvs.width; i+= cbg.getImage().width)
                 ctx.drawImage(bg,i,0);
             
             
@@ -277,7 +356,7 @@ window.onload = function() {
 
     }
 
-    images = loadImages(imageNames);
+    // images = loadImages(imageNames);
     
     loadIndividualImages();
 
